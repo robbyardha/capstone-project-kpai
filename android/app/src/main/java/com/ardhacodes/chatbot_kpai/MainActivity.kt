a9f416f7d95d6b9233f6a0392b5990b73905da4c
robbyardha <robbyardha@gmail.com>
2021-05-20 21:16:00 +0700

    add android

---
+++ android/app/src/main/java/com/ardhacodes/chatbot_kpai/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ardhacodes.chatbot_kpai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
